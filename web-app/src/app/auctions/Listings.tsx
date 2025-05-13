9c5664e77b4c8f744a484d30e27d10bd8d7378ed
LeXuanPhuoc-Lab <stevenmarks147@gmail.com>
2025-05-13 14:25:16 +0700

    Add use params store, fitlers, pagination

--- web-app/src/app/auctions/Listings.tsx
+++ web-app/src/app/auctions/Listings.tsx
@@ -1,40 +1,66 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import AuctionCard from './AuctionCard';
-import { Auction } from '../../../types';
+import { Auction, PagedResult } from '../../../types';
 import AppPagination from '../components/AppPagination';
 import { getData } from '../actions/auctionActions';
-
+import Filters from './Filters';
+import { useParamsStore } from '../hooks/useParamsStore';
+import { useShallow } from 'zustand/shallow';
+import qs from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
 export default function Listings() {
-    const [auctions, setAuctions] = useState<Auction[]>([]);
-    const [pageIndex, setPageIndex] = useState(1);
-    const [totalPage, setTotalPage] = useState(0);
+    const [data, setData] = useState<PagedResult<Auction>>();
+    const params = useParamsStore(useShallow(state => ({
+        pageIndex: state.pageIndex,
+        pageSize: state.pageSize,
+        pageCount: state.pageCount,
+        searchValue: state.searchValue,
+        orderBy: state.orderBy,
+        filterBy: state.filterBy
+    })));
+    const setParams = useParamsStore(state => state.setParams);
+    const queryUrl = qs.stringifyUrl({ url: '', query: params });
+
+    function setPageNumber(pageIndex: number) {
+        setParams({ pageIndex });
+    }
 
     useEffect(() => {
-        getData({pageIndex}).then(res => {
-            setAuctions(res.data.result);
-            setTotalPage(res.data.totalPage);
+        console.log(queryUrl)
+        getData(queryUrl).then(res => {
+            console.log(queryUrl);
+            setData(res.data);
         });
-    }, [pageIndex]);
+    }, [queryUrl])
 
-    if(auctions.length === 0) return <h3>Loading...</h3>
+    if (!data) return <h3>Loading...</h3>;
 
     return (
         <>
-            <div className='grid grid-cols-4 gap-6'>
-                {auctions && auctions.map(auction => (
-                    <AuctionCard auction={auction} key={auction.id} />
-                ))};
-            </div>
-            <div className='flex justify-center mt-4'>
-                <AppPagination 
-                    currentPage={pageIndex}
-                    totalPage={totalPage}
-                    onPageChanged={setPageIndex}
-                />
-            </div>
+            <Filters />
+            {data.result.length === 0 ? (
+                <EmptyFilter showReset />
+            ) : (
+                <>
+                    <div className='grid grid-cols-4 gap-6'>
+                        {(data.result && data.result.length > 0 &&
+                            data.result.map((auction: Auction) => (
+                                <AuctionCard auction={auction} key={auction.id} />
+                            ))
+                        )}
+                    </div>
+                    <div className='flex justify-center mt-4'>
+                        <AppPagination
+                            currentPage={params.pageIndex}
+                            totalPage={data.totalPage}
+                            onPageChanged={setPageNumber}
+                        />
+                    </div>
+                </>
+            )}
         </>
     )
 }
--- web-app/src/app/nav/Navbar.tsx
+++ web-app/src/app/nav/Navbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Search from './Search'
+import Logo from './Logo'
 import { AiOutlineCar } from 'react-icons/ai'
 
 export default function Navbar() {
@@ -6,11 +8,8 @@ export default function Navbar() {
     <header className='
       sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
     '>
-      <div className='flex items-center gap-2 text-3xl font-semibold text-red-500'>
-        <AiOutlineCar size={34} />
-        <div>Car Auctions</div>
-      </div>
-      <div>Search</div>
+      <Logo />
+      <Search />
       <div>Right</div>
     </header>
   )
